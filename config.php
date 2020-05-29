82c195472009042b2c708fe01ffb8c0c07dd04f9
aleksa993 <49314470+aleksa993@users.noreply.github.com>
2020-05-29 15:04:38 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'huaweiu8500');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'boombox');
 
 //Pokusaj konekcije ka Mysql bazi//
